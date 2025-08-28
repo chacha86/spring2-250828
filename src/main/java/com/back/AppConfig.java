50bc3ab62b996b1824f388954fa40b3c0eb96dc5
chacha86 <taejincha0619@gmail.com>
2025-08-28 15:11:37 +0900

    work

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,14 +1,20 @@
 package com.back;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
-//    @Bean
-//    public PersonService personService() {
-//        PersonService p1 = new PersonService();
-//        return p1;
-//    }
+
+    @Bean
+    public PersonRepository personRepository() {
+        return new PersonRepository(1);
+    }
+
+    @Bean
+    public PersonRepository personRepositoryV2() {
+        return new PersonRepository(2);
+    }
 
 }
