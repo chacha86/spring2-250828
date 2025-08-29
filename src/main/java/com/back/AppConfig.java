1518e6703fad1f65a65810f9558b98711452e558
chacha86 <taejincha0619@gmail.com>
2025-08-29 10:15:55 +0900

    work

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -3,47 +3,24 @@ package com.back;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public PersonRepository personRepository() {
-        return new PersonRepository(1);
+    public ApplicationRunner myApplicationRunner3() {
+        return args -> {
+               work1();
+               work2();
+        };
     }
 
-    @Bean
-    public PersonRepository personRepositoryV2() {
-        return new PersonRepository(2);
+    public void work1() {
+        System.out.println("work1");
     }
 
-    @Bean
-    @Order(2)
-    public ApplicationRunner myApplicationRunner1() {
-        return new MyApplicationRunner(1);
+    public void work2() {
+        System.out.println("work2");
     }
-
-    @Bean
-    @Order(1)
-    public ApplicationRunner myApplicationRunner2() {
-        return new MyApplicationRunner(2);
-    }
-
-//    @Bean
-//    public ApplicationRunner myApplicationRunner3() {
-//        return args -> {
-//               work1();
-//               work2();
-//        };
-//    }
-//
-//    public void work1() {
-//        System.out.println("work1");
-//    }
-//
-//    public void work2() {
-//        System.out.println("work2");
-//    }
 
 }
